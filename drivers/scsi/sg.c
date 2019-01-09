3ae52f4e2e6b1f56aa8a077c325643a48dfa0409
GuaiYiHu <guaiyihu@foxmail.com>
2019-01-10 01:49:04 +0800

    drivers: scsi: Drop upstream changes
    Change-Id: I182193c6fb60d54947ada254f6164d5ea46909a5

--- drivers/scsi/sg.c
+++ drivers/scsi/sg.c
@@ -6,7 +6,6 @@
  *
  * Original driver (sg.c):
  *        Copyright (C) 1992 Lawrence Foard
- * Copyright (C) 2018 XiaoMi, Inc.
  * Version 2 and 3 extensions to driver:
  *        Copyright (C) 1998 - 2014 Douglas Gilbert
  *
@@ -2052,12 +2051,11 @@ sg_get_rq_mark(Sg_fd * sfp, int pack_id)
 		if ((1 == resp->done) && (!resp->sg_io_owned) &&
 		    ((-1 == pack_id) || (resp->header.pack_id == pack_id))) {
 			resp->done = 2;	/* guard against other readers */
-			write_unlock_irqrestore(&sfp->rq_list_lock, iflags);
-			return resp;
+			break;
 		}
 	}
 	write_unlock_irqrestore(&sfp->rq_list_lock, iflags);
-	return NULL;
+	return resp;
 }
 
 /* always adds to end of list */
